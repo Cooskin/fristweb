a2448038c3c71c0904be1bf25e8d99dc7032e30b
Cooskin <1575437818@qq.com>
2019-12-24 00:02:48 +0800

    未完成

--- 传一自由行/js/index.js
+++ 传一自由行/js/index.js
@@ -4,6 +4,24 @@ var oBannerNav = oBanner.getElementsByClassName('banner_nav')[0];
 var aImgLi = oBannerImg.children;
 var aNavLi = oBannerNav.children;
 
+var bannerNum = 0;
+setInterval(function() {
+    bannerNum++;
+    if (bannerNum >= aImgLi.length) {
+        bannerNum = 0;
+    }
+    banner();
+}, 3000)
+
+function banner() {
+    for (var i = 0; i < aNavLi.length; i++) {
+        aNavLi[i].className = '';
+        aImgLi[i].className = '';
+    }
+    aNavLi[bannerNum].className = 'activ';
+    aImgLi[bannerNum].className = 'dis';
+}
+
 for (var i = 0; i < aNavLi.length; i++) {
     aNavLi[i].index = i;
     aNavLi[i].onclick = function() {
@@ -12,6 +30,7 @@ for (var i = 0; i < aNavLi.length; i++) {
             aImgLi[j].className = '';
         }
         this.className = 'activ';
-        aImgLi[this.index].className = 'activ';
+        aImgLi[this.index].className = 'dis';
+        bannerNum = this.index
     }
 }
