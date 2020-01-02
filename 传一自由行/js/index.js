8694980994744e3054393d748b042549f82c56a9
Cooskin <1575437818@qq.com>
2020-01-02 17:28:04 +0800

    01.02

--- 传一自由行/js/index.js
+++ 传一自由行/js/index.js
@@ -55,7 +55,8 @@ for (let i = 0; i < aNavLi.length; i++) {
 var travelsBtn = document.getElementById('travels_head');
 var aTravelsLi = travelsBtn.getElementsByTagName('li');
 
-function travels(image, title, text, country, userImage, userName, view, ding) {
+function travels(ravelsId, image, title, text, country, userImage, userName, view, ding) {
+    this.ravelsId = ravelsId;
     this.image = image;
     this.title = title;
     this.text = text;
@@ -66,6 +67,7 @@ function travels(image, title, text, country, userImage, userName, view, ding) {
     this.ding = ding;
 }
 var travels1 = new travels(
+    `1`,
     '<img src="./images/content/travels.jpg" alt="">',
     '美国旧金山大桥',
     '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
@@ -76,6 +78,7 @@ var travels1 = new travels(
     0
 );
 var travels2 = new travels(
+    `2`,
     '<img src="./images/content/travels.jpg" alt="">',
     '美国旧金山大桥',
     '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
@@ -86,6 +89,7 @@ var travels2 = new travels(
     1
 );
 var travels3 = new travels(
+    `3`,
     '<img src="./images/content/travels.jpg" alt="">',
     '美国旧金山大桥',
     '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
@@ -95,10 +99,98 @@ var travels3 = new travels(
     '250/11',
     3
 );
-var travelsArr = [travels1, travels2, travels3];
+var travels4 = new travels(
+    `4`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    10
+);
+var travels5 = new travels(
+    `5`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    0
+);
+var travels6 = new travels(
+    `6`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    3
+);
+var travels7 = new travels(
+    `7`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    1
+);
+var travels8 = new travels(
+    `8`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    12
+);
+var travels9 = new travels(
+    `9`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    20
+);
+var travels10 = new travels(
+    `10`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    1
+);
+var travels11 = new travels(
+    `11`,
+    '<img src="./images/content/travels.jpg" alt="">',
+    '美国旧金山大桥',
+    '炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！炸了炸了！心态爆炸了！',
+    `美国`,
+    `<img src="./images/content/travels.jpg" alt="">`,
+    '心态爆炸',
+    '250/11',
+    6
+);
+
+var travelsArr = [travels1, travels2, travels3, travels4, travels5, travels6, travels7, travels8, travels9, travels10, travels11];
 
 var oldTravelsArr = JSON.stringify(travelsArr);
-var newTravelsArr = JSON.parse(localStorage.getItem('HX191110_travelsArr'));
 localStorage.setItem('HX191110_travelsArr', oldTravelsArr);
 
 var oInfo = document.getElementsByClassName('travels')[0];
@@ -119,20 +211,32 @@ function bubbleSort(arr) {
     return arr;
 }
 
-function newArr(arr) {
-    for (var i = arr.length - 1; i >= 0; i--) {
-
+function newTravels(arr) {
+    var newArr = [];
+    var len = arr.length;
+    for (var i = len - 1; i >= 0; i--) {
+        newArr.push(arr[i])
     }
+    return newArr;
 }
-console.log(bubbleSort(travelsArr))
-rintingTravels(bubbleSort(newTravelsArr), newTravelsArr);
 
-function rintingTravels(arr, newArr) {
+var rintingTravelsArr = JSON.parse(localStorage.getItem('HX191110_travelsArr'));
+var newAry = newTravels(rintingTravelsArr);
+
+var sort = bubbleSort(rintingTravelsArr);
+rintingTravels(1, sort);
+
+function rintingTravels(page, arr) {
     var ravelsStr = '';
-    if (aTravelsLi[0].className == 'activ') {
+    var min = (page - 1) * 3;
+    var max = page * 3;
+    if (max > arr.length) {
+        max = arr.length;
+    }
+    oDingBtn = oInfo.getElementsByClassName('ding_btn');
 
-        for (let i = 0; i < arr.length; i++) {
-            ravelsStr += `<li>
+    for (let i = min; i < max; i++) {
+        ravelsStr += `<li>
         <a class="travels_img" href="javascript:;">` + arr[i].image + `</a>
         <h3>
             <a href="javascript:;">` + arr[i].title + `</a>
@@ -145,41 +249,74 @@ function rintingTravels(arr, newArr) {
         <a class="user_name" href="javascript:;">` + arr[i].userName + `</a>
         <a class="page_view" href="javascript:;">` + arr[i].view + `</a>
         <a class="ding" href="javascript:;">` + arr[i].ding + `</a>
-        <a class="ding_btn" href="javascript:;"></a>
+        <a class="ding_btn" index = ` + arr[i].ravelsId + ` href="javascript:;"></a>
     </li>`
-        }
-        aUl.innerHTML = ravelsStr;
-    } else {
 
-        for (let i = newArr.length - 1; i >= 0; i--) {
-            ravelsStr += `<li>
-        <a class="travels_img" href="javascript:;">` + newArr[i].image + `</a>
-        <h3>
-            <a href="javascript:;">` + newArr[i].title + `</a>
-        </h3>
-        <p>
-            <a href="javascript:;">` + newArr[i].text + `</a>
-        </p>
-        <a class="place" href="javascript:;">` + newArr[i].country + `</a><em>，by</em>
-        <a class="user_img" href="javascript:;">` + newArr[i].userImage + `</a>
-        <a class="user_name" href="javascript:;">` + newArr[i].userName + `</a>
-        <a class="page_view" href="javascript:;">` + newArr[i].view + `</a>
-        <a class="ding" href="javascript:;">` + newArr[i].ding + `</a>
-        <a class="ding_btn" href="javascript:;"></a>
-    </li>`
-        }
-        aUl.innerHTML = ravelsStr;
     }
+    aUl.innerHTML = ravelsStr;
 
+    for (let i = min; i < max; i++) {
+        oDingBtn[i].onclick = function() {
+            for (let j = 0; j < arr.length; j++) {
+                if (this.index == arr[j].index) {
+                    if () {
+
+                    }
+                    arr[j].ding += 1;
+                }
+            }
+            this.index
+            alert(j)
+
+
+        }
+    }
 }
 
-// var hotTravelsArr =
 for (let i = 0; i < aTravelsLi.length; i++) {
     aTravelsLi[i].onclick = function() {
         for (let j = 0; j < aTravelsLi.length; j++) {
             aTravelsLi[j].className = '';
         }
         this.className = 'activ';
-        rintingTravels(bubbleSort(newTravelsArr), newTravelsArr);
+        if (aTravelsLi[0].className == 'activ') {
+            rintingTravels(1, sort);
+        } else {
+            rintingTravels(1, newAry);
+            console.log(newAry)
+        }
+    }
+}
+paging(rintingTravelsArr);
+
+function paging(arr) {
+    var oPage = document.getElementsByClassName('page')[0].getElementsByTagName('ul')[0];
+    // 判断并计算最大的页数
+    var num = 0;
+    if (arr.length % 3 == 0) {
+        num = arr.length / 3;
+    } else {
+        num = Math.ceil(arr.length / 3);
+    }
+    // 先清空原有的页数
+    oPage.innerHTML = '';
+    for (var i = 0; i < num; i++) {
+        // 创造节点，形成每一个分页
+        var oLi = document.createElement('li');
+        // 给每一个分页标序号
+        oLi.innerHTML = i + 1;
+
+        oLi.onclick = function() {
+            if (aTravelsLi[0].className == 'activ') {
+                rintingTravels(this.innerHTML, sort);
+            } else {
+                rintingTravels(this.innerHTML, newAry);
+                console.log(newAry)
+            }
+        }
+
+        // 将每一个分页节点追加到ul当中
+        oPage.appendChild(oLi);
+
     }
 }
